2c145fc593f67d4d3908bbdc3a1aa7ac29c7556e
yaquelin2305 <ya.rugel@duocuc.cl>
2026-01-17 11:10:47 -0300

    Actualización microservicio registros salud

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import registrosRoutes from './routes/registros.routes.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}))
+
 app.use(express.json())
 
 app.use('/api/registros', registrosRoutes)
